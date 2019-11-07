41accd88505a32ba774dc2c3bfe8d463cc43fb21
Balsakup <balsakup@gmail.com>
2019-11-07 20:01:08 +0100

    :sparkles: Commencement des catégories (#4)

--- resources/admin/views/layouts/elements/sidebar.blade.php
+++ resources/admin/views/layouts/elements/sidebar.blade.php
@@ -6,6 +6,12 @@
                     <span class="nav-icon fa fa-tachometer-alt"></span> Tableau de bord
                 </a>
             </li>
+            <li class="nav-title">Blog</li>
+            <li class="nav-item">
+                <a href="{{ route('admin::categories.index') }}" class="nav-link">
+                    <span class="nav-icon fa fa-tags"></span> Catégories
+                </a>
+            </li>
         </ul>
     </nav>
     <button class="sidebar-minimizer brand-minimizer" type="button"></button>
--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoriesController;
 use App\Http\Controllers\Admin\DashboardController;
 
 Route::get('/', [ DashboardController::class, 'home' ])->name('dashboard.home');
+
+Route::resource('categories', CategoriesController::class);
